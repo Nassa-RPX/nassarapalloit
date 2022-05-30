99d8735ed77b4a25ead446a180a2afc7e9005081
Giovanni Costa <cgiovanni1996@gmail.com>
2022-05-30 22:32:54 +0200

    :sparkles: feat: mantine initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
+import { Button, Title } from "@mantine/core";
 import type { NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>nassarapallo</title>
         <meta name="description" content="Nassa Rapallo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>Hello</main>
-    </div>
+      <main>
+        <Title order={1}>Nassa Rapallo</Title>
+      </main>
+    </>
   );
 };
 
