afda6ccd6f41e67a596fde24021c9722207f9bec
Giovanni Costa <cgiovanni1996@gmail.com>
2022-05-30 21:32:20 +0200

    :sparkles: feat: nextjs initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>nassarapallo</title>
+        <meta name="description" content="Nassa Rapallo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Hello</main>
+    </div>
+  );
+};
+
+export default Home;
